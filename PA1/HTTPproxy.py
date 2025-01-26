61c0735418808c4cb087c869a2a109d6ffcce2c5
Tim Lawrence <timothyjl2002@gmail.com>
2025-01-26 00:44:45 -0700

    Clean and comment

--- PA1/HTTPproxy.py
+++ PA1/HTTPproxy.py
@@ -16,52 +16,51 @@ class ParseError(Enum):
     BADREQ = 2
 
 def receive_request(client_skt: socket) -> bytes:
+    '''Handles a client connection and receives an HTTP request.'''
     request: bytes = b''
     # TODO: recv loop
     request = client_skt.recv(2048)
-    logging.info(request)
     return request
 
 def parse_request(message: bytes) -> tuple[ParseError, bytes, int, bytes, dict[bytes, bytes]]:
+    '''Parses a received HTTP request and extracts host, port, path, and headers.
+    Returns a ParseError if the request is malformed or can't be processed.'''
     host, port, path, headers = None, None, None, None
 
     logging.debug('start parse')
     try:
+        # Parse header into basic tokens
         message_tokens = message.split(maxsplit=3)
         assert len(message_tokens) >= 3
         logging.debug('correct length')
 
+        # Check method
         method = message_tokens[0]
         assert method in [b'GET', b'HEAD', b'OPTIONS', b'TRACE', b'PUT', b'DELETE', b'POST', b'PATCH', b'CONNECT']
         if method != b'GET':
             return ParseError.NOTIMPL, None, None, None, None
         logging.debug('correct method')
         
+        # Parse URL
         url = message_tokens[1]
         url_match = re.fullmatch(rb"http:\/\/([^:]+?)(?::(\d+)|)(/.*)", url)
         assert url_match
         host = url_match[1]
         port = int(url_match[2]) if url_match[2] else 80
         path = url_match[3]
-        logging.debug(f"host: {host}")
-        logging.debug(f"port: {port}")
-        logging.debug(f"path: {path}")
-        logging.debug('correct url')
 
+        # Check protocol
         assert message_tokens[2] == b'HTTP/1.0'
-        logging.debug('correct protocol')
 
+        # Parse headers
         headers = {}
         if len(message_tokens) == 4: # Additional headers exist
             message_headers = message_tokens[-1].split(b'\r\n')
-            for mh in message_headers:
-                if not mh: break
-                assert re.match(rb'\S+: .+', mh)
-                mh_split = mh.split(b': ', maxsplit=1)
-                mh_key, mh_value = (i.strip() for i in mh_split)
+            for header_line in message_headers:
+                if not header_line: break # ignore trailing empty string
+                assert re.match(rb'\S+: .+', header_line)
+                mh_key, mh_value = (i.strip() for i in header_line.split(b': ', maxsplit=1))
                 headers[mh_key] = mh_value
-                logging.debug(f'added kvpair {mh_key}: {mh_value}')
-        logging.debug(f'correct headers: {headers}')
 
     except AssertionError:
         logging.debug('fail parse')
@@ -70,26 +69,30 @@ def parse_request(message: bytes) -> tuple[ParseError, bytes, int, bytes, dict[b
         logging.debug('end parse')
         return None, host, port, path, headers
 
-def send_to_server(host: bytes, port: int, path: bytes, headers: dict) -> bytes:
+def request_server(host: bytes, port: int, path: bytes, headers: dict) -> bytes:
+    '''Reconstructs and passes an HTTP request to the intended host.
+    Closes connection after receipt.'''
     with socket(AF_INET, SOCK_STREAM) as server_skt:
         server_skt.connect((host, port))
         
-        header_str = f"GET http://{host.decode()}{path.decode()} HTTP/1.0\r\nConnection: close\r\n"
+        header_str = f"GET http://{host.decode()}{path.decode()} HTTP/1.0\r\n"
+        headers[b'Connection'] = b'close' # Override or add connection info to header
         for headkey, headval in headers.items():
             header_str += f"{headkey}: {headval}\r\n"
         header_str += "\r\n"
-        logging.debug(header_str)
         server_skt.send(header_str.encode())
 
+        # TODO: recv loop
         response = server_skt.recv(2048)
-        logging.info(response)
         return response
     
 def send_client_response(client_skt: socket, response: bytes):
+    '''Sends a server response back to the client and closes the connection.'''
     client_skt.send(response)
     client_skt.close()
 
 def send_client_error(client_skt: socket, errormsg: str):
+    '''Sends an error to the client if their request could not be processed.'''
     error_header = f"HTTP/1.0 {errormsg}\r\n\r\n"
     client_skt.send(error_header.encode())
     client_skt.close()
@@ -112,26 +115,26 @@ def main():
     # Set up signal handling (ctrl-c)
     signal.signal(signal.SIGINT, ctrl_c_pressed)
 
-    # TODO: Set up sockets to receive requests
+    # Set up socket to receive requests
     listener_skt = socket(AF_INET, SOCK_STREAM)
     listener_skt.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # Make the autograder behave
     listener_skt.bind((address, port))
     listener_skt.listen()
 
+    # Accept client sockets and handle requests
     while True:
         client_skt, client_address = listener_skt.accept()
         message = receive_request(client_skt)
         error, host, port, path, headers = parse_request(message)
         if not error:
-            response = send_to_server(host, port, path, headers)
+            response = request_server(host, port, path, headers)
             send_client_response(client_skt, response)
         elif error == ParseError.BADREQ:
             send_client_error(client_skt, "400 Bad Request")
         elif error == ParseError.NOTIMPL:
-            logging.debug("NOTIMPL")
             send_client_error(client_skt, "501 Not Implemented")
 
 # Set logging level
-logging.basicConfig(level=logging.DEBUG)        
+logging.basicConfig(level=logging.ERROR)        
 if __name__ == '__main__':
     main()
