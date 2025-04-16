31011048a60a56a1aa7f04917883837c4a690178
Tim Lawrence <timothyjl2002@gmail.com>
2025-04-16 00:25:42 -0600

    Fix network setup mk2

--- PA3/host/entrypoint.sh
+++ PA3/host/entrypoint.sh
@@ -4,9 +4,9 @@ HOSTNAME=$(hostname)
 
 # Configure default route
 if [ "$HOSTNAME" = "ha" ]; then
-    ip route add default via 10.0.14.4
+    ip route replace default via 10.0.14.4
 elif [ "$HOSTNAME" = "hb" ]; then
-    ip route add default via 10.0.15.4
+    ip route replace default via 10.0.15.4
 fi
 
 # Keep container running
