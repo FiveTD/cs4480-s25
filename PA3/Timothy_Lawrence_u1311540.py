90e5602c21769b2f4131d4941a9547f0c9584338
Tim Lawrence <timothyjl2002@gmail.com>
2025-04-21 04:49:42 -0600

    Improve OSPF converge wait time Add quit option

--- PA3/Timothy_Lawrence_u1311540.py
+++ PA3/Timothy_Lawrence_u1311540.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Simple OSPF network orchestrator using FRR
+# Simple OSPF network orchestrator using FRRouting
 # Written by Tim Lawrence for CS4480, Spring 2025
 
 import argparse
@@ -10,15 +10,17 @@ import time
 def parse_args() -> argparse.Namespace | None:
     '''Parse command line arguments. If no arguments are provided,
     prints the help message and returns `None`.'''
-    parser = argparse.ArgumentParser(description="Simple OSPF network orchestrator using FRR")
-    parser.add_argument('-c', '--construct', help="construct the network topology",
+    parser = argparse.ArgumentParser(description='Simple OSPF network orchestrator using FRRouting')
+    parser.add_argument('-c', '--construct', help='construct the network topology',
                        action='store_true')
-    parser.add_argument('-d', '--daemon', help="Start and config OSPF daemon",
+    parser.add_argument('-d', '--daemon', help='start and config OSPF daemon',
                        action='store_true')
-    parser.add_argument('-r', '--route', help="Install host routing",
+    parser.add_argument('-r', '--route', help='set host routing',
                        action='store_true')
-    parser.add_argument('-p', '--path', help="Set the preferred network traffic path",
+    parser.add_argument('-p', '--path', help='set the preferred network traffic path',
                        choices=('north', 'south'))
+    parser.add_argument('-q', '--quit', help='shut down and deconstruct the network',
+                        action='store_true')
     
     args = parser.parse_args()
     if not any(vars(args).values()): # If no option, print help message
@@ -28,7 +30,7 @@ def parse_args() -> argparse.Namespace | None:
 
 def construct_network():
     '''Constructs the network topology using Docker containers.'''
-    print("ORCH: Constructing network")
+    print('ORCH: Constructing network')
     
     os.system('docker compose up -d')
     
@@ -56,7 +58,7 @@ def set_host_routes():
             print('.done')
             break
         else:
-            print('.', end='')
+            print('.', end='', flush=True)
             time.sleep(1)
         
 def set_preferred_path(path: str):
@@ -68,6 +70,12 @@ def set_preferred_path(path: str):
     for i in (1, 3):
         os.system(f'docker exec -it r{i} vtysh -c \'configure terminal\' ' +
                   f'-c \'interface eth2\' -c\'ip ospf cost {cost}\' -c \'end\'')
+        
+def close_network():
+    '''Shuts down the network and cleans up the docker setup.'''
+    print('ORCH: Shutting down the network')
+    
+    os.system('docker compose down')
 
 def main():
     args = parse_args()
@@ -77,6 +85,7 @@ def main():
     if args.daemon: start_ospf_daemon()
     if args.route: set_host_routes()
     if args.path: set_preferred_path(args.path)
+    if args.quit: close_network()
 
 if __name__ == '__main__':
     main()
